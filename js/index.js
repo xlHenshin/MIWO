81016d216bdff75930bad5cf32643c00dde8b828
xlHenshin <andreslayer117@gmail.com>
2022-05-01 14:57:15 -0500

    index script

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const signInBtn = document.getElementById("signin");
+const signUpBtn = document.getElementById("signup");
+
+signInBtn.addEventListener('click', ()=>{
+
+    console.log("SIGNIN");
+})
+
+signUpBtn.addEventListener('click', ()=>{
+
+    console.log("SIGNUP");
+})
