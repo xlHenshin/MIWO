42cab24b039e05b9b0e2a7c29c3f5da44e5057b7
xlHenshin <andreslayer117@gmail.com>
2022-05-01 18:53:17 -0500

    corrigiendo de nuevo esa mierda

--- js/index.js
+++ js/index.js
@@ -4,7 +4,8 @@ const signUpBtn = document.getElementById("signup");
 signInBtn.addEventListener('click', ()=>{
 
     console.log("SIGNIN");
-    window.location.href = "../ingresar.html"
+    window.location.href = "./ingresar.html";
+    console.log("DONEEEE")
 })
 
 signUpBtn.addEventListener('click', ()=>{
