6b826b086a61bfc143e3c01298963e7c7a4389a7
xlHenshin <andreslayer117@gmail.com>
2022-05-01 18:28:10 -0500

    ingresar con limite en los inputs

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ const signUpBtn = document.getElementById("signup");
 signInBtn.addEventListener('click', ()=>{
 
     console.log("SIGNIN");
+    window.location.href = "./ingresar.html"
 })
 
 signUpBtn.addEventListener('click', ()=>{
