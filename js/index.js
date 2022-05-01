c865630bebd0833b5314f1ad1d8235b46225c792
xlHenshin <andreslayer117@gmail.com>
2022-05-01 18:31:43 -0500

    un hpta punto

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const signUpBtn = document.getElementById("signup");
 signInBtn.addEventListener('click', ()=>{
 
     console.log("SIGNIN");
-    window.location.href = "./ingresar.html"
+    window.location.href = "../ingresar.html"
 })
 
 signUpBtn.addEventListener('click', ()=>{
