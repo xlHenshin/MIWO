3d7abc303badc2d440f171004378b8f6d637275a
xlHenshin <andreslayer117@gmail.com>
2022-05-03 00:40:00 -0500

    inputs hasta registro 3

---
+++ js/registro.js
@@ -0,0 +1,9 @@
+const documentType = document.getElementById("documentType");
+const documentNumber = document.getElementById("documentNumber");
+const nextBtn = document.getElementById("nextBtn");
+const signInBtn = document.getElementById("signin");
+
+signInBtn.addEventListener('click', ()=>{
+
+    window.location.href = "./ingresar.html";
+})
