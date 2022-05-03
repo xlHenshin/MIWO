ce90e41347fd623e7fbd684416f5ff76a7bb4898
xlHenshin <andreslayer117@gmail.com>
2022-05-03 00:54:03 -0500

    registro hasta el 6

---
+++ js/registro4.js
@@ -0,0 +1,7 @@
+const celularRegister = document.getElementById("celularRegister");
+const nextBtn = document.getElementById("nextBtn");
+
+nextBtn.addEventListener('click', ()=>{
+
+    window.location.href = "./registro5.html";
+})
