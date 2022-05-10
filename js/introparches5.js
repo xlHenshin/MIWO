f304974e260e9484b429950f08408c9d4df02415
xlHenshin <andreslayer117@gmail.com>
2022-05-10 07:11:27 -0500

    tutorial de parches

---
+++ js/introparches5.js
@@ -0,0 +1,12 @@
+const nextBtn = document.getElementById("nextBtn");
+const backBtn = document.getElementById("backBtn");
+
+nextBtn.addEventListener('click', ()=>{
+
+    window.location.href = "./parches-principal.html";
+})
+
+backBtn.addEventListener('click', ()=>{
+
+    window.location.href = "./introparches4.html";
+})
