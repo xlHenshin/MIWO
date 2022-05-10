568b7055cae695d76ff5516d7f407fb5900cdef1
xlHenshin <andreslayer117@gmail.com>
2022-05-10 07:30:33 -0500

    pantallas del home conectadas

---
+++ js/home-mi-dinero.js
@@ -0,0 +1,18 @@
+const parchesNav = document.getElementById("parchesNav");
+const homeSubscripciones = document.getElementById("homeSubscripciones");
+const homeAmiwos = document.getElementById("homeAmiwos");
+
+parchesNav.addEventListener('click', ()=>{
+
+    window.location.href = "./parches-principal.html";
+})
+
+homeSubscripciones.addEventListener('click', ()=>{
+
+    window.location.href = "./home-subscripciones.html";
+})
+
+homeAmiwos.addEventListener('click', ()=>{
+
+    window.location.href = "./home-amiwos.html";
+})
---
+++ js/parches-principal.js
@@ -0,0 +1,12 @@
+const homeNav = document.getElementById("homeNav");
+const backBtn = document.getElementById("backBtn");
+
+homeNav.addEventListener('click', ()=>{
+
+    window.location.href = "./home-mi-dinero.html";
+})
+
+backBtn.addEventListener('click', ()=>{
+
+    window.location.href = "./introparches2.html";
+})
