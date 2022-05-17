56bb9217d3dde8c074db914219894ac24db210d0
xlHenshin <andreslayer117@gmail.com>
2022-05-17 11:16:50 -0500

    inputs solicitar prestamo

---
+++ js/solprestamo.js
@@ -0,0 +1,18 @@
+const paratuparcheBtn = document.getElementById("paratuparcheBtn");
+const paramiBtn = document.getElementById("paramiBtn");
+const solicitarBtn = document.getElementById("solicitarBtn");
+
+paratuparcheBtn.addEventListener('click', (e) => {
+
+    console.log("Para tu parche")
+})
+
+paramiBtn.addEventListener('click', (e) => {
+
+    console.log("Sólo para mi")
+})
+
+solicitarBtn.addEventListener('click', (e) => {
+
+    console.log("solicitar")
+})
